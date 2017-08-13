9ee3bf130d3f9634b4cb385f40074e0775073e3a
Muhammad Kaisar Arkhan <yuki@coala.io>
2017-08-13 16:00:30 +0700

    .emacs.d: Total makeover

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,39 +1,25 @@
-;;; init.el --- Yuki's early Emacs init
-;; Author: Muhammad Kaisar Arkhan (yuki_is_bored)
+(package-initialize)
 
-;;; Commentary:
-;; Inspired by https://github.com/edvorg/emacs-configs
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 
-;;; Code:
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives
+	       '("gnu" . "http://elpa.gnu.org/packages/")))
 
-;; GET OF MY LAWN REEEEEEE
-
-;(package-initialize)
-
-(require 'package)
-
-;; Constants
-
-(defconst my-init-dir "~/.emacs.d/init.d")
-(defconst my-custom-file "~/.emacs.d/custom.el")
-
-;; Turn off startup screen
-
-(setq inhibit-splash-screen t)
-(setq inhibit-startup-message t)
-
-;; Please don't mess my shit
-
-(setq auto-save-default nil)
-(setq make-backup-files nil)
-(setq custom-file my-custom-file)
-;(setq select-enable-clipboard nil)
-(setq-default indent-tabs-mode nil)
-(load custom-file 'noerr)
-
-;; Hook real initialization script
-
-(add-hook 'after-init-hook (lambda ()
-                             (load "~/.emacs.d/init-real.el")))
-
-;;; init.el ends here
+(org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default)))
+ '(package-selected-packages (quote (req-package use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
