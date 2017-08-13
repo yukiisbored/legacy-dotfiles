7784cb195a0c6ecf9b89dbdf420eda3ecd7a7a8b
Muhammad Kaisar Arkhan <yuki@coala.io>
2017-08-13 16:05:27 +0700

    init.el: Throw custom away

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,19 +7,10 @@
   (add-to-list 'package-archives
 	       '("gnu" . "http://elpa.gnu.org/packages/")))
 
+                                        ;
+;; GET OF MY LAWN REEEEEEEE
+(defconst my-custom-file "~/.emacs.d/custom.el")
+(setq custom-file my-custom-file)
+(load custom-file 'noerr)
+
 (org-babel-load-file "~/.emacs.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default)))
- '(package-selected-packages (quote (req-package use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
